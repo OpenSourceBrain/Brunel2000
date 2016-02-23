7cda349b43ea9816dd9aa9a3dfbe08e2060874f0
Padraig Gleeson <p.gleeson@gmail.com>
2016-02-23 14:11:19 +0000

    Updated tests for pynn

---
+++ PyNN/brunel_test.py
@@ -0,0 +1,17 @@
+"""
+A scaled down version of the Brunel model useful for testing (see OMV files: .test.*)
+
+"""
+
+from brunel08 import runBrunelNetwork
+
+from pyNN.utility import get_script_args
+
+simulator_name = get_script_args(1)[0]
+simtime = 500
+order = 100
+
+eta         = 2.0     # rel rate of external input
+g           = 5.0
+
+runBrunelNetwork(g=g, eta=eta, simtime = simtime, order = order, save=True, simulator_name=simulator_name)
