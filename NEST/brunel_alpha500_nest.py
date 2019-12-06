879603f95b61ffdeb9d160ae625ceb5276f61671
Padraig Gleeson <p.gleeson@gmail.com>
2019-12-06 17:26:49 +0000

    Test 500 cell example; test on xenial; test nest 2.18

---
+++ NEST/brunel_alpha500_nest.py
@@ -0,0 +1,9 @@
+from brunel_alpha_nest import runBrunelNetwork
+
+simtime = 1000
+order = 100
+
+eta         = 2.0     # rel rate of external input
+g           = 5.0
+
+runBrunelNetwork(g=g, eta=eta, simtime = simtime, order = order, save=True, N_rec=500)
